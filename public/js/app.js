c950092ad27a78e1646af932487db437e281a118
Justin Dennison <justin1dennison@gmail.com>
2016-03-12 01:28:17 +0000

    fixed app.js controller typo

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ angular.module('todoApp', [
 angular.module('todoApp') //this is the getter syntax, we can use this so we don't have to make a variable to store our angular application; YAYYY no globals
       .controller('TodoController', TodoController);
 
-TodoContoller.$inject = ['$scope', '$http'];
+TodoController.$inject = ['$scope', '$http'];
 
 function TodoController($scope, $http){
 
