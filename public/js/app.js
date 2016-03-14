f5ddff9e6576b7f27c74cf460668eca4829fa8a8
Noam <noamg@cox.net>
2016-03-14 14:34:08 -0400

    mean_todo

--- public/js/app.js
+++ public/js/app.js
@@ -9,5 +9,52 @@ angular.module('todoApp') //this is the getter syntax, we can use this so we don
 TodoController.$inject = ['$scope', '$http'];
 
 function TodoController($scope, $http){
+  $scope.todos = [];
+  $scope.isEditing = false;
+  initTodos();
 
+  $scope.saveTodo = function(){
+    $http.post('/api/todos', $scope.newTodo)
+    .then(function(response){
+      initTodos();
+      $scope.newTodo = {};
+    })
+    .catch(function(err){
+      console.err(err);
+    });
+}
+  $scope.editTodo = function(todo){
+    $scope.isEditing = !$scope.isEditing;
+    $scope.editingTodo = todo;
+  }
+  $scope.updateTodo = function(todo){
+    $http.put('/api/todos/'+todo._id, todo)
+    .then(function(response){
+      $scope.isEditing = false;
+      initTodos();
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+  }
+  function initTodos(){
+    $http.get('/api/todos')
+        .then(function(response){
+          console.log(response);
+          $scope.todos = response.data;
+        })
+        .catch(function(err){
+          console.err(err);
+        });
+  }
+  $scope.deleteTodo = function(todo){
+    var id = todo._id;
+    $http.delete('/api/todos/'+id)
+    .then(function(response){
+      initTodos();
+    })
+    .catch(function(err){
+      console.err(err);
+    });
+  }
 }
--- server.js
+++ server.js
@@ -1,33 +1,76 @@
 var express = require('express'),
     logger  = require('morgan')('dev'),
     path    = require('path'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bodyParser = require('body-parser')
     server  = express();
 
+    var todoSchema = new Schema({
+      desc: {
+        type: String,
+        required: true
+      },
+      completed: {
+        type: Boolean,
+        required: true
 
+      },
+    });
+
+    var Todo = mongoose.model('Todo', todoSchema);
+    mongoose.connect('mongodb://localhost/todoApp');
 var port = process.env.PORT || 9000;
 
 server.use(express.static(path.join(__dirname,'public')));
 server.use(logger);
-
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 
 server.get('/', function(req, res){
-  res.send('this is a starter application, welcome!');
+  res.sendFile('public/html/index.html', {root: __dirname});
 });
 
-server.get('/json', function(req, res){
-  var someJson = [
-    {
-      desc: 'learn how to build a web based api',
-      completed: false
-    },
-    {
-      desc: 'build an Angular frontend that consumes the api',
-      completed: false
-    }
-  ];
+server.get('/api/todos', function(req, res){
+  Todo.find(function(err, todos){
+    if(err) throw err;
+    res.json(todos);
+  });
+});
 
-  res.json(someJson);
+server.post('/api/todos', function(req,res){
+  var desc = req.body.desc;
+  var todoObj = {
+    desc: desc,
+    completed: false
+  };
+  Todo.create(todoObj, function(err, todo){
+    if(err) throw err;
+    res.json(todo);
+  });
+});
+
+server.put('/api/todos/:id', function(req, res){
+  var id = req.params.id;
+  var desc = req.body.desc;
+  var completed =  req.body.completed;
+  var update = {
+    desc: desc,
+    completed: completed,
+  };
+  Todo.findOneAndUpdate({_id: id}, update, {new: true}, function(err, todo){
+    if(err) throw err;
+
+    res.json(todo);
+  });
+});
+
+server.delete('/api/todos/:id', function(req, res){
+  Todo.findOneAndRemove({_id: req.params.id}, function(err, todo){
+   if (err) throw err;
+   res.json(todo);
+  });
 });
 
 server.listen(port, function(){
